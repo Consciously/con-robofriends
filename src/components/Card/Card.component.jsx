978bb1811d88e74912cffd1b6d35d3b9cb4c940e
Stefan Ihle <stefanihle@gmx.net>
2022-09-16 11:14:32 +0200

    chamge css for images

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -3,12 +3,14 @@ import './Card.styles.css';
 const Card = ({ robot: { id, name, email } }) => {
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img
-				src={`https://robohash.org/${id}?200x200`}
-				alt='robots'
-				width='200'
-				height='200'
-			/>
+			<div className='image-container'>
+				<img
+					src={`https://robohash.org/${id}?200x200`}
+					alt='robots'
+					width='200'
+					height='200'
+				/>
+			</div>
 
 			<div>
 				<h2>{name}</h2>
