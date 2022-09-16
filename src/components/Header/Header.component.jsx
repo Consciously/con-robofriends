a209562d426fa6ee397ac16529803578717c2c6c
Stefan Ihle <stefanihle@gmx.net>
2022-09-16 10:06:06 +0200

    remove unnecessary code

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,12 +1,10 @@
 import React, { Fragment, memo } from 'react';
-import CountButton from '../CountButton/CountButton.component';
 
 const Header = memo(() => {
 	console.log('Header');
 	return (
 		<Fragment>
 			<h1 className='f1'>RoboFriends</h1>
-			<CountButton style={{ color: 'tomato' }} />
 		</Fragment>
 	);
 });
